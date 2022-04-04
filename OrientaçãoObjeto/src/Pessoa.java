df0d8fbeae51905c8d65e0641c2f480b2685f1cf
Aluno <Aluno@DSN1251230723>
2022-04-04 11:23:43 -0300

    Programa para troca de figurinhas

---
+++ OrientaçãoObjeto/src/Pessoa.java
@@ -0,0 +1,14 @@
+
+public class Pessoa {
+	String nome;
+	int numfig;
+	
+	void receber(int numfig) {
+		this.numfig = numfig; 		
+	}
+	
+	void dar(int numfig) {
+
+	}
+	
+}
---
+++ OrientaçãoObjeto/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa();
+		
+		pessoa1.nome = "Vinicius";
+		pessoa1.numfig = 1;
+		
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numfig);
+		
+	}
+
+}
