b3828e985044719713c8df0ef5747e0e4a4c32fd
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-02 10:21:26 -0300

    Adição de elementos de comparação

---
+++ ModificadorAcesso/src/Cliente.java
@@ -0,0 +1,58 @@
+import java.util.Objects;
+
+public class Cliente {
+	private String nome;
+	private int cpf;
+	private int numConta;
+	
+	public Cliente() {
+		
+	}
+
+	public String getNome() { //o get serve para mostrar informações, enquanto o set serve para inserir informações
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getNumConta() {
+		return numConta;
+	}
+
+	public void setNumConta(int numConta) {
+		this.numConta = numConta;
+	}
+
+	@Override
+	public String toString() {  //esse método exibe todas as informações de uma vez só 
+		return "\n Cliente: \n Nome = " + nome + "\n CPF = " + cpf + "\n Número da conta = " + numConta;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, nome, numConta);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return cpf == other.cpf && Objects.equals(nome, other.nome) && numConta == other.numConta;
+	}
+	
+}
--- ModificadorAcesso/src/main.java
+++ ModificadorAcesso/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		ContaBancaria conta1 = new ContaBancaria(true, 3.0);
+//		ContaBancaria conta1 = new ContaBancaria(true, 3.0);
 			
 //		conta1.ativo = true;
 //		conta1.numConta = 1234;
@@ -13,14 +13,55 @@ public class main {
 //		System.out.println(conta1.numConta);
 //		System.out.println("R$" + conta1.saldo);
 		
-		conta1.receber(0);
-		conta1.dar(0);
-		conta1.saldo();
+//		conta1.receber(0);
+//		conta1.dar(0);
+//		conta1.saldo();
+//		
+//		System.out.println(conta1.saldo());
+//
+//		ContaBancaria conta2 = new ContaBancaria();
+//		System.out.println(conta2.saldo());
 		
-		System.out.println(conta1.saldo());
+		Cliente cliente1 = new Cliente();
+				
+		cliente1.setNome("Vinicius");
+		cliente1.setCpf(121522362);
+		cliente1.setNumConta(212362);
+		
+		System.out.println(cliente1);
+		
+		Cliente cliente2 = new Cliente();
+		
+		cliente2.setNome("Pedro");
+		cliente2.setCpf(965214326);
+		cliente2.setNumConta(321025);
+		
+		System.out.println(cliente2);
+		
+		if (cliente1 == cliente2) { //serve para comparar objetos
+			System.out.println("\n Os objetos são iguais");
+		}
+		else {
+			System.out.println("\n Os objetos são diferentes");
+	    }
+		
+		
+		if (cliente1.getNome() == cliente2.getNome()) {
+			System.out.println(" Os nomes são iguais");
+		}
+		else {
+			System.out.println(" Os nomes são diferentes");
+		}
 
-		ContaBancaria conta2 = new ContaBancaria();
-		System.out.println(conta2.saldo());
+		
+		if(cliente1.equals(cliente2)) { //serve para comparar atributos
+			System.out.println(" Os atributos são iguais");
+		}
+		else {
+			System.out.println(" Os atributos são diferentes");
+		}
+		
+		
 	}
 
 }
