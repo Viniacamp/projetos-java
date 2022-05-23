a22f6c67bace2262ea139a449b7e7290e81dd75f
Vinicius S. <69431917+Viniacamp@users.noreply.github.com>
2022-05-23 11:16:45 -0300

    Programa mostrando passo a passo a troca de uma lâmpada

---
+++ TrocarLâmpada/src/Lampada.java
@@ -0,0 +1,24 @@
+
+public class Lampada {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Olá Mundo! (para quem não é bilingue)");
+		System.out.println("-----------");
+		System.out.println("Certificar-se de que não está passando corrente na lâmpada, para evitar choque");
+		System.out.println("Pegar uma escada");
+		System.out.println("Subir na escada");
+		System.out.println("Retirar a lâmpada queimada");
+		System.out.println("Descer da escada");
+		System.out.println("Pegar a lâmpada nova");
+		System.out.println("Subir novamente na escada");
+		System.out.println("Colocar a lâmpada nova");
+		System.out.println("Descer da escada");
+		System.out.println("Acionar o interruptor");
+		System.out.println("Verificar se a lâmpada está funcinando");
+		System.out.println("Se a lâmpada não estiver funcionando, troque-a e repita o processo");
+		System.out.println("Guardar a escada");		
+	}
+
+}
+     
