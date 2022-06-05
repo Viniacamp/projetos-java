31e7720a394374f59d868a801bfe92dd14992643
Vinicius S <68025681+Viniacamp@users.noreply.github.com>
2022-06-05 19:03:21 -0300

    Programa que faz o uso de interfaces
    Adição de comentários ao projeto

--- Interfaces/src/Carro.java
+++ Interfaces/src/Carro.java
@@ -1,5 +1,5 @@
 
-//quando uma interface for herdar de outra, usa-se o comando extends
+//quando uma interface (ou classe) for herdar de outra, usa-se o comando extends
 public interface Carro extends Automovel{
 
 	public void abrirPorta();
