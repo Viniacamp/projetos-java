8f3ac184492d3e27fc4ab033bb931285fd3aa612
Vinicius S <68025681+Viniacamp@users.noreply.github.com>
2022-06-06 11:24:25 -0300

    Programa de preparação de bebidas (com uso de polimorfismo)
    Adição de comentários ao código

--- PolimorfismoBebidas/src/Preparacao.java
+++ PolimorfismoBebidas/src/Preparacao.java
@@ -1,18 +1,19 @@
 
 public class Preparacao { 
+	
 	public void prepararBebida(Bebida bebida) { 
-		//preparando e indicando qual sera a bebida
+		
 		System.out.println("Preparando " + bebida.getNome());
-		bebida.preparar(); //declarando que uma bebida vai ser preparada
-		if (bebida.isAquecer()) { //se a bebida for aquecida a mensagem que ela esta aquecendo irá aparecer
+		bebida.preparar();
+		
+		if (bebida.isAquecer()) { //se a bebida precisar ser aquecida, a mensagem abaixo irá ser exibida
 			System.out.println("Aquecendo " + bebida.getNome());
-		}// se não só ignora e add a bebida ao copo
-		System.out.println("Servir " + bebida.getNome() + " no copo "); 
+		}
+		
+		System.out.println("Servir " + bebida.getNome() + " no copo "); //getNome = puxa o nome da bebida criada
 		System.out.println("Bebida pronta!");
 		System.out.println("-- -- -- -- -- -- -- -- -- -- -- ");
-		
-	}
 	
-	
+	}	
 	
 }
