34b8550282afd1dab129de7330f1792194d63f6c
Vinicius S <68025681+Viniacamp@users.noreply.github.com>
2022-06-06 11:11:54 -0300

    Programa de preparação de bebidas (com uso de polimorfismo)
    Adição de comentários ao código

--- PolimorfismoBebidas/src/Aplicacao.java
+++ PolimorfismoBebidas/src/Aplicacao.java
@@ -1,11 +1,12 @@
 
 public class Aplicacao {
 
-	public static void main(String[] args) { // não precisa criar um objeto preparador se ja é static 
+	public static void main(String[] args) { 
 
-		Preparacao preparacao = new Preparacao(); //construindo um obj
-		LeiteQuente leiteQuente = new LeiteQuente();
-		preparacao.prepararBebida(leiteQuente);  //mostrando como se fosse o passo a passo da bebida que tem de ser preparada
+		Preparacao preparacao = new Preparacao(); //criação do objeto preparacao
+		LeiteQuente leiteQuente = new LeiteQuente(); //criação de um novo objeto leiteQuente
+		preparacao.prepararBebida(leiteQuente);  //o objeto preparação chama o método prepararBebida, que recebe o valor (objeto) leiteQuente
+		//(que é a bebida que vai ser preparada)
 		
 		Refrigerante refrigerante = new Refrigerante();
 		preparacao.prepararBebida(refrigerante);
